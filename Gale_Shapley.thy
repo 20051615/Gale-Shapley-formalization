fc590b8b221cd6baf06723ffa73ec078d1a9fc95
Jason Yue <yuehaiyin990427@gmail.com>
2021-02-07 23:56:49 +0800

    Initial commit of GS theory file: key next step: certify lexicographic termination

---
+++ Gale_Shapley.thy
@@ -0,0 +1,55 @@
+theory Gale_Shapley
+  imports Main
+begin
+type_synonym person = "nat"
+type_synonym man = "person"
+type_synonym woman = "person"
+type_synonym pref_matrix = "(person list) list"
+type_synonym matching = "(woman option) list"
+
+fun findFreeMan::"matching \<Rightarrow> man option" where
+"findFreeMan [] = None" |
+"findFreeMan (None # ops) = Some 0" |
+"findFreeMan ((Some w) # ops) = (case findFreeMan ops of None \<Rightarrow> None | Some m \<Rightarrow> Some (m + 1))"
+
+fun findFiance::"matching \<Rightarrow> woman \<Rightarrow> man option" where
+"findFiance [] _ = None" |
+"findFiance (None # ops) w = (case (findFiance ops w) of None \<Rightarrow> None | Some m \<Rightarrow> Some (m + 1))" |
+"findFiance ((Some w1) # ops) w = (if w1 = w then (Some 0) else
+                             (case (findFiance ops w) of None \<Rightarrow> None | Some m \<Rightarrow> Some (m + 1)))"
+
+fun progress_prop_idxs::"nat list \<Rightarrow> man \<Rightarrow> nat list" where
+"progress_prop_idxs next_prop_idxs m = list_update next_prop_idxs m (next_prop_idxs!m+1)"
+
+fun find_idx::"person list \<Rightarrow> person \<Rightarrow> nat" where
+"find_idx [] p = 0" |
+"find_idx (p1 # ps) p = (if p = p1 then 0 else 1 + find_idx ps p)"
+
+fun prefers::"person \<Rightarrow> pref_matrix \<Rightarrow> person \<Rightarrow> person \<Rightarrow> bool" where
+"prefers p PPrefs p1 p2 = (let PPref = PPrefs!p in
+                          find_idx PPref p1 < find_idx PPref p2)"
+
+fun Gale_Shapley'::"nat \<Rightarrow> pref_matrix \<Rightarrow> pref_matrix
+ \<Rightarrow> matching \<Rightarrow> nat list \<Rightarrow>
+ matching" where
+"Gale_Shapley' N MPrefs WPrefs 
+ engagements next_prop_idxs 
+ = (case findFreeMan engagements of 
+  None \<Rightarrow> engagements |
+  Some m \<Rightarrow> (let w = (MPrefs!m)!(next_prop_idxs!m) in (
+    case findFiance engagements w of
+      None \<Rightarrow> Gale_Shapley' N MPrefs WPrefs 
+        (list_update engagements m (Some w)) (progress_prop_idxs next_prop_idxs m)
+    | Some m' \<Rightarrow> (
+      if prefers w WPrefs m m' then Gale_Shapley' N MPrefs WPrefs
+        (list_update (list_update engagements m (Some w)) m' None) (progress_prop_idxs next_prop_idxs m)
+      else Gale_Shapley' N MPrefs WPrefs
+        engagements (progress_prop_idxs next_prop_idxs m)
+    )
+  ))
+)"
+
+fun Gale_Shapley::"pref_matrix \<Rightarrow> pref_matrix \<Rightarrow> matching" where
+"Gale_Shapley MPrefs WPrefs = (let N = length MPrefs in
+ Gale_Shapley' N MPrefs WPrefs (replicate N None) (replicate N 0))"
+end
