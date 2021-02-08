3d794568178f5e996de9581ec0b03e8c59e27ade
Jason Yue <yuehaiyin990427@gmail.com>
2021-02-08 09:19:36 +0800

    Thought about termination for a bit

--- Gale_Shapley.thy
+++ Gale_Shapley.thy
@@ -18,9 +18,6 @@ fun findFiance::"matching \<Rightarrow> woman \<Rightarrow> man option" where
 "findFiance ((Some w1) # ops) w = (if w1 = w then (Some 0) else
                              (case (findFiance ops w) of None \<Rightarrow> None | Some m \<Rightarrow> Some (m + 1)))"
 
-fun progress_prop_idxs::"nat list \<Rightarrow> man \<Rightarrow> nat list" where
-"progress_prop_idxs next_prop_idxs m = list_update next_prop_idxs m (next_prop_idxs!m+1)"
-
 fun find_idx::"person list \<Rightarrow> person \<Rightarrow> nat" where
 "find_idx [] p = 0" |
 "find_idx (p1 # ps) p = (if p = p1 then 0 else 1 + find_idx ps p)"
@@ -33,20 +30,24 @@ fun Gale_Shapley'::"nat \<Rightarrow> pref_matrix \<Rightarrow> pref_matrix
  \<Rightarrow> matching \<Rightarrow> nat list \<Rightarrow>
  matching" where
 "Gale_Shapley' N MPrefs WPrefs 
- engagements next_prop_idxs 
- = (case findFreeMan engagements of 
-  None \<Rightarrow> engagements |
-  Some m \<Rightarrow> (let w = (MPrefs!m)!(next_prop_idxs!m) in (
-    case findFiance engagements w of
-      None \<Rightarrow> Gale_Shapley' N MPrefs WPrefs 
-        (list_update engagements m (Some w)) (progress_prop_idxs next_prop_idxs m)
-    | Some m' \<Rightarrow> (
-      if prefers w WPrefs m m' then Gale_Shapley' N MPrefs WPrefs
-        (list_update (list_update engagements m (Some w)) m' None) (progress_prop_idxs next_prop_idxs m)
-      else Gale_Shapley' N MPrefs WPrefs
-        engagements (progress_prop_idxs next_prop_idxs m)
-    )
-  ))
+ engagements prop_idxs 
+ = 
+(if sum_list prop_idxs \<ge> N * N then engagements else
+  (case findFreeMan engagements of 
+   None \<Rightarrow> engagements |
+   Some m \<Rightarrow> (let w = (MPrefs!m)!(prop_idxs!m);
+     next_prop_idxs = list_update prop_idxs m (Suc (prop_idxs!m)) in (
+     case findFiance engagements w of
+       None \<Rightarrow> Gale_Shapley' N MPrefs WPrefs 
+         (list_update engagements m (Some w)) next_prop_idxs
+     | Some m' \<Rightarrow> (
+       if prefers w WPrefs m m' then Gale_Shapley' N MPrefs WPrefs
+         (list_update (list_update engagements m (Some w)) m' None) next_prop_idxs
+       else Gale_Shapley' N MPrefs WPrefs
+         engagements next_prop_idxs
+     )
+   ))
+  )
 )"
 
 fun Gale_Shapley::"pref_matrix \<Rightarrow> pref_matrix \<Rightarrow> matching" where
